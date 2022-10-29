dc504db73d30bac6b583258daf2f958a1b74d3ee
TI-IT <go.tiit.ogle@gmail.com>
2022-10-29 13:05:10 +0300

    Action  [need build client]  [need deploy server]

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>*** KvantNoLimit ***</h1>
+      <h1>KvantNoLimit</h1>
+      <h3>Приветствую LILIT</h3>
     </div>
   );
 }
