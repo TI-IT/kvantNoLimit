c63137f2c639871a752911067833fbe3098e81da
Tigran <go.tiit.ogle@gmail.com>
2022-12-03 22:02:25 +0300

    google_finish
    [need build client]
    
    [need deploy server]

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Admin from './pages/Admin';
 
 
 function App() {
-  const server_host = process.env.NODE_ENV === 'development' ? 'http://localhost:9001' : 'https://api.kvantnolimit.ru';
+  const server_host = process.env.NODE_ENV === 'development ' ? 'http://localhost:9001' : 'https://api.kvantnolimit.ru';
  
   return (
     <BrowserRouter>
